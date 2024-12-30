b67d39bc5912983ec1fb5ea47846e64e732a5113
rizwan-shaikh-01 <rizwanshaikhakhtar314@gmail.com>
2024-12-30 15:23:13 +0530

    learning-if-else

---
+++ day-14/test.py
@@ -0,0 +1,11 @@
+a=int(input("if 1 kg biryani is rupee "))
+b=int(input("my budget is "))
+c=int(a+b)
+
+if (a<=b):
+    print("you can buy biryani ")
+
+else:
+    print("you can't buy biryani")
+
+    
