5679fc58b5d951d03f16d1d7c679ab1254d99530
rizwan-shaikh-01 <rizwanshaikhakhtar314@gmail.com>
2024-12-31 21:55:03 +0530

    time-codes

---
+++ day-15/good-morning-sir-if-else-test.py
@@ -0,0 +1,15 @@
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+
+name = input('Enter your name: ')
+recenttime = time.strftime('%H:%M:%S')
+Recenttime = int(time.strftime('%H'))
+c= name.capitalize()
+if(4<=Recenttime<12):
+    print('GOOD MORNING',c,'its',recenttime)
+elif(12>=Recenttime<17):
+    print('GOOD EVENING',c,'its',recenttime)
+else:
+    print('GOOD NIGHT',c,'its',recenttime)
+# print('Hii',name,'its',recenttime)
